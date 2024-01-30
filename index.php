bab4feb3342b348ac9478354975d47fd2892c24b
Arismário Neves <arismarioneves@users.noreply.github.com>
2024-01-30 00:19:44 -0300

    PIX

--- index.php
+++ index.php
@@ -1,90 +1,92 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
-	<head>
-		<!--
-			▓▓▓▓Dev by Mari05liM▓▓▓▓
-			Mari05liM
-			mariodev@outlook.com.br
-			 -->
-		<meta charset="UTF-8">
-		<title>LORDE&gt;FRIO</title>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
-		<meta http-equiv="Pragma" content="no-cache">
-		<meta http-equiv="Expires" content="0">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<meta name="author" content="Mari05liM">
-		<meta name="contact" content="mariodev@outlook.com.br">
-		<meta name="description" content="">
-		<link rel="icon" href="favicon.ico">
-		<link rel="stylesheet" href="style.css">
-	</head>
-	<body>
-		<div id="root">
-			<header class="app-header">
-				<div class="wrapper">
-					<h1>
-						<a href="#">
-							<img src="logo.png" width="30">
-						</a>
-					</h1>
-					<nav>
-						<a href="#summary" class="">Sobre</a>
-						<!-- <button class="button" onclick="window.open('https://github.com/arismarioneves/LordeFrio/', '_blank')">Playground</button> -->
-						<a class="icon-link" href="https://github.com/arismarioneves/LordeFrio/" target="_blank" title="Github">
-                        	<svg width="24" height="24" viewBox="0 0 24 24" class="icon" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        	    <path fill-rule="evenodd" clip-rule="evenodd" d="M12 0C5.37 0 0 5.37 0 12C0 17.31 3.435 21.795 8.205 23.385C8.805 23.49 9.03 23.13 9.03 22.815C9.03 22.53 9.015 21.585 9.015 20.58C6 21.135 5.22 19.845 4.98 19.17C4.845 18.825 4.26 17.76 3.75 17.475C3.33 17.25 2.73 16.695 3.735 16.68C4.68 16.665 5.355 17.55 5.58 17.91C6.66 19.725 8.385 19.215 9.075 18.9C9.18 18.12 9.495 17.595 9.84 17.295C7.17 16.995 4.38 15.96 4.38 11.37C4.38 10.065 4.845 8.985 5.61 8.145C5.49 7.845 5.07 6.615 5.73 4.965C5.73 4.965 6.735 4.65 9.03 6.195C9.99 5.925 11.01 5.79 12.03 5.79C13.05 5.79 14.07 5.925 15.03 6.195C17.325 4.635 18.33 4.965 18.33 4.965C18.99 6.615 18.57 7.845 18.45 8.145C19.215 8.985 19.68 10.05 19.68 11.37C19.68 15.975 16.875 16.995 14.205 17.295C14.64 17.67 15.015 18.39 15.015 19.515C15.015 21.12 15 22.41 15 22.815C15 23.13 15.225 23.505 15.825 23.385C18.2072 22.5808 20.2773 21.0498 21.7438 19.0074C23.2103 16.9651 23.9994 14.5143 24 12C24 5.37 18.63 0 12 0Z" fill="currentColor">
-                        	    </path>
-                        	</svg>
-                        </a>
-					</nav>
-				</div>
-			</header>
-			<section id="hero">
-				<h2>
-					<strong><em>LORDE&gt;</em>FRIO</strong>
-				</h2>
-			</section>
-			<section id="summary">
-				<div class="wrapper">
-					<div class="summary-card">
-						<div class="card">
-							<svg width="16" height="16" viewBox="0 0 16 16" class="icon" fill="none" xmlns="http://www.w3.org/2000/svg">
-								<path fill-rule="evenodd" clip-rule="evenodd" d="M0 5.25143C0 4.3552 0.356318 3.58403 1.06897 2.93791C1.75863 2.31263 2.62069 2 3.65517 2C4.71265 2 5.5862 2.31263 6.27586 2.93791L7.34483 3.87582C7.41379 3.93835 7.41379 4.00088 7.34483 4.06341L6.65517 4.65742C6.58621 4.71994 6.52874 4.71994 6.48276 4.65742L5.41379 3.7195C4.90804 3.30265 4.32184 3.09423 3.65517 3.09423C3.01149 3.09423 2.43678 3.30265 1.93104 3.7195C1.47126 4.13636 1.24138 4.64699 1.24138 5.25143C1.24138 5.85586 1.47126 6.3665 1.93104 6.78335C2.43678 7.2002 3.01149 7.40862 3.65517 7.40862C4.32184 7.40862 4.90804 7.2002 5.41379 6.78335L9.72414 2.93791C10.4138 2.31263 11.2874 2 12.3448 2C13.3793 2 14.2414 2.31263 14.931 2.93791C15.6437 3.58403 16 4.3552 16 5.25143C16 6.14766 15.6437 6.91882 14.931 7.56494C14.7464 7.73238 14.5493 7.8774 14.3399 8C14.5493 8.1226 14.7464 8.26762 14.931 8.43506C15.6437 9.08118 16 9.85234 16 10.7486C16 11.6448 15.6437 12.416 14.931 13.0621C14.2414 13.6874 13.3793 14 12.3448 14C11.2874 14 10.4138 13.6874 9.72414 13.0621L8.65517 12.1242C8.58621 12.0616 8.58621 11.9991 8.65517 11.9366L9.34483 11.3426C9.41379 11.2801 9.47126 11.2801 9.51724 11.3426L10.5862 12.2805C11.092 12.6973 11.6782 12.9058 12.3448 12.9058C12.9885 12.9058 13.5632 12.6973 14.069 12.2805C14.5517 11.8428 14.7931 11.3322 14.7931 10.7486C14.7931 10.165 14.5517 9.65435 14.069 9.21665C13.5632 8.7998 12.9885 8.59138 12.3448 8.59138C11.6782 8.59138 11.092 8.7998 10.5862 9.21665L6.27586 13.0621C5.5862 13.6874 4.72414 14 3.68966 14C2.65517 14 1.79311 13.6874 1.10345 13.0621C0.367812 12.3951 0 11.624 0 10.7486C0 9.85234 0.356318 9.08118 1.06897 8.43506C1.25874 8.263 1.46157 8.11462 1.67745 7.98991C1.47442 7.86946 1.28309 7.72781 1.10345 7.56494C0.367814 6.89798 0 6.12681 0 5.25143ZM5.6835 8.00072C5.89337 7.87797 6.09082 7.73271 6.27586 7.56494L10.5862 3.7195C11.092 3.30265 11.6782 3.09423 12.3448 3.09423C12.9885 3.09423 13.5632 3.30265 14.069 3.7195C14.5517 4.1572 14.7931 4.66783 14.7931 5.25143C14.7931 5.83502 14.5517 6.34565 14.069 6.78335C13.5632 7.2002 12.9885 7.40862 12.3448 7.40862C11.6782 7.40862 11.092 7.2002 10.5862 6.78335L9.51724 5.84544C9.47126 5.78291 9.41379 5.78291 9.34483 5.84544L8.65517 6.43945C8.58621 6.50198 8.58621 6.5645 8.65517 6.62703L9.72414 7.56494C9.90882 7.73238 10.1067 7.8774 10.3177 8C10.1067 8.1226 9.90882 8.26762 9.72414 8.43506L5.41379 12.2805C4.90804 12.6973 4.32184 12.9058 3.65517 12.9058C3.01149 12.9058 2.43678 12.6973 1.93103 12.2805C1.47126 11.8636 1.24138 11.353 1.24138 10.7486C1.24138 10.1441 1.47126 9.6335 1.93103 9.21665C2.43678 8.7998 3.01149 8.59138 3.65517 8.59138C4.32184 8.59138 4.90804 8.7998 5.41379 9.21665L6.48276 10.1546C6.52874 10.2171 6.58621 10.2171 6.65517 10.1546L7.34483 9.56055C7.41379 9.49802 7.41379 9.4355 7.34483 9.37297L6.27586 8.43506C6.09154 8.26795 5.89409 8.12317 5.6835 8.00072Z" fill="currentColor"></path>
-							</svg>
-							<div class="stat-text">
-								<?php
-								$visitas = file_get_contents("contador.txt");
-								$visitas = $visitas + 1;
-								$fp = fopen("contador.txt", "w");
-								fwrite($fp, $visitas);
-								fclose($fp);
-								?>
-								<div>aproximadamente</div>
-								<div class="number"><?php echo $visitas; ?></div>
-								<div>visitantes até agora</div>
-							</div>
-							<div class="stat-text"></div>
-						</div>
-					</div>
-					<div class="blabla">
-						<h3>Ajude a combater o LORDE><i>FRIO</i></h3>
-						<h4>Seja um <b>Herói</b>!</h4>
-						<pre><code>Chave PIX: </code><br><code class="import-url">db0c57cf-95a8-4f7b-a3f9-102e03621afb</code></pre>
-						<div class="run"></div>
-					</div>
-				</div>
-			</section>
-			<footer>
-				<div class="wrapper">
-					<p><span class="line"></span></p>
-					<p><em>Mari05liM</em> | <a href="mailto:mariodev@outlook.com.br">mariodev@outlook.com.br</a></p>
-					<p style="font-size:20px"></p>
-					<p style="margin-top: 2rem;">
-						<button class="instatus"><span class="status-dot" style="background-color: rgb(15, 186, 85);"></span><span class="status-text">Online</span></button>
-					</p>
-				</div>
-			</footer>
-		</div>
-	</body>
+
+<head>
+    <!--
+        ▓▓▓▓Dev by Mari05liM▓▓▓▓
+        Mari05liM
+        mariodev@outlook.com.br
+    -->
+    <meta charset="UTF-8">
+    <title>LORDE&gt;FRIO</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Expires" content="0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="author" content="Mari05liM">
+    <meta name="contact" content="mariodev@outlook.com.br">
+    <meta name="description" content="">
+    <link rel="icon" href="favicon.ico">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div id="root">
+        <header class="app-header">
+            <div class="wrapper">
+                <h1>
+                    <a href="#">
+                        <img src="logo.png" width="30">
+                    </a>
+                </h1>
+                <nav>
+                    <a href="#summary" class="">Sobre</a>
+                    <a class="icon-link" href="https://github.com/arismarioneves/LordeFrio/" target="_blank" title="Github">
+                        <svg width="24" height="24" viewBox="0 0 24 24" class="icon" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M12 0C5.37 0 0 5.37 0 12C0 17.31 3.435 21.795 8.205 23.385C8.805 23.49 9.03 23.13 9.03 22.815C9.03 22.53 9.015 21.585 9.015 20.58C6 21.135 5.22 19.845 4.98 19.17C4.845 18.825 4.26 17.76 3.75 17.475C3.33 17.25 2.73 16.695 3.735 16.68C4.68 16.665 5.355 17.55 5.58 17.91C6.66 19.725 8.385 19.215 9.075 18.9C9.18 18.12 9.495 17.595 9.84 17.295C7.17 16.995 4.38 15.96 4.38 11.37C4.38 10.065 4.845 8.985 5.61 8.145C5.49 7.845 5.07 6.615 5.73 4.965C5.73 4.965 6.735 4.65 9.03 6.195C9.99 5.925 11.01 5.79 12.03 5.79C13.05 5.79 14.07 5.925 15.03 6.195C17.325 4.635 18.33 4.965 18.33 4.965C18.99 6.615 18.57 7.845 18.45 8.145C19.215 8.985 19.68 10.05 19.68 11.37C19.68 15.975 16.875 16.995 14.205 17.295C14.64 17.67 15.015 18.39 15.015 19.515C15.015 21.12 15 22.41 15 22.815C15 23.13 15.225 23.505 15.825 23.385C18.2072 22.5808 20.2773 21.0498 21.7438 19.0074C23.2103 16.9651 23.9994 14.5143 24 12C24 5.37 18.63 0 12 0Z" fill="currentColor">
+                            </path>
+                        </svg>
+                    </a>
+                </nav>
+            </div>
+        </header>
+        <section id="hero">
+            <h2>
+                <strong><em>LORDE&gt;</em>FRIO</strong>
+            </h2>
+        </section>
+        <section id="summary">
+            <div class="wrapper">
+                <div class="summary-card">
+                    <div class="card">
+                        <svg width="16" height="16" viewBox="0 0 16 16" class="icon" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M0 5.25143C0 4.3552 0.356318 3.58403 1.06897 2.93791C1.75863 2.31263 2.62069 2 3.65517 2C4.71265 2 5.5862 2.31263 6.27586 2.93791L7.34483 3.87582C7.41379 3.93835 7.41379 4.00088 7.34483 4.06341L6.65517 4.65742C6.58621 4.71994 6.52874 4.71994 6.48276 4.65742L5.41379 3.7195C4.90804 3.30265 4.32184 3.09423 3.65517 3.09423C3.01149 3.09423 2.43678 3.30265 1.93104 3.7195C1.47126 4.13636 1.24138 4.64699 1.24138 5.25143C1.24138 5.85586 1.47126 6.3665 1.93104 6.78335C2.43678 7.2002 3.01149 7.40862 3.65517 7.40862C4.32184 7.40862 4.90804 7.2002 5.41379 6.78335L9.72414 2.93791C10.4138 2.31263 11.2874 2 12.3448 2C13.3793 2 14.2414 2.31263 14.931 2.93791C15.6437 3.58403 16 4.3552 16 5.25143C16 6.14766 15.6437 6.91882 14.931 7.56494C14.7464 7.73238 14.5493 7.8774 14.3399 8C14.5493 8.1226 14.7464 8.26762 14.931 8.43506C15.6437 9.08118 16 9.85234 16 10.7486C16 11.6448 15.6437 12.416 14.931 13.0621C14.2414 13.6874 13.3793 14 12.3448 14C11.2874 14 10.4138 13.6874 9.72414 13.0621L8.65517 12.1242C8.58621 12.0616 8.58621 11.9991 8.65517 11.9366L9.34483 11.3426C9.41379 11.2801 9.47126 11.2801 9.51724 11.3426L10.5862 12.2805C11.092 12.6973 11.6782 12.9058 12.3448 12.9058C12.9885 12.9058 13.5632 12.6973 14.069 12.2805C14.5517 11.8428 14.7931 11.3322 14.7931 10.7486C14.7931 10.165 14.5517 9.65435 14.069 9.21665C13.5632 8.7998 12.9885 8.59138 12.3448 8.59138C11.6782 8.59138 11.092 8.7998 10.5862 9.21665L6.27586 13.0621C5.5862 13.6874 4.72414 14 3.68966 14C2.65517 14 1.79311 13.6874 1.10345 13.0621C0.367812 12.3951 0 11.624 0 10.7486C0 9.85234 0.356318 9.08118 1.06897 8.43506C1.25874 8.263 1.46157 8.11462 1.67745 7.98991C1.47442 7.86946 1.28309 7.72781 1.10345 7.56494C0.367814 6.89798 0 6.12681 0 5.25143ZM5.6835 8.00072C5.89337 7.87797 6.09082 7.73271 6.27586 7.56494L10.5862 3.7195C11.092 3.30265 11.6782 3.09423 12.3448 3.09423C12.9885 3.09423 13.5632 3.30265 14.069 3.7195C14.5517 4.1572 14.7931 4.66783 14.7931 5.25143C14.7931 5.83502 14.5517 6.34565 14.069 6.78335C13.5632 7.2002 12.9885 7.40862 12.3448 7.40862C11.6782 7.40862 11.092 7.2002 10.5862 6.78335L9.51724 5.84544C9.47126 5.78291 9.41379 5.78291 9.34483 5.84544L8.65517 6.43945C8.58621 6.50198 8.58621 6.5645 8.65517 6.62703L9.72414 7.56494C9.90882 7.73238 10.1067 7.8774 10.3177 8C10.1067 8.1226 9.90882 8.26762 9.72414 8.43506L5.41379 12.2805C4.90804 12.6973 4.32184 12.9058 3.65517 12.9058C3.01149 12.9058 2.43678 12.6973 1.93103 12.2805C1.47126 11.8636 1.24138 11.353 1.24138 10.7486C1.24138 10.1441 1.47126 9.6335 1.93103 9.21665C2.43678 8.7998 3.01149 8.59138 3.65517 8.59138C4.32184 8.59138 4.90804 8.7998 5.41379 9.21665L6.48276 10.1546C6.52874 10.2171 6.58621 10.2171 6.65517 10.1546L7.34483 9.56055C7.41379 9.49802 7.41379 9.4355 7.34483 9.37297L6.27586 8.43506C6.09154 8.26795 5.89409 8.12317 5.6835 8.00072Z" fill="currentColor"></path>
+                        </svg>
+                        <div class="stat-text">
+                            <?php
+                            $visitas = file_get_contents("contador.txt");
+                            $visitas = $visitas + 1;
+                            $fp = fopen("contador.txt", "w");
+                            fwrite($fp, $visitas);
+                            fclose($fp);
+                            ?>
+                            <div>aproximadamente</div>
+                            <div class="number"><?php echo $visitas; ?></div>
+                            <div>visitantes até agora</div>
+                        </div>
+                        <div class="stat-text"></div>
+                    </div>
+                </div>
+                <div class="blabla">
+                    <h3>Ajude a combater o LORDE><i>FRIO</i></h3>
+                    <h4>Seja um <b>Herói</b>!</h4>
+                    <pre><code>Chave PIX: </code><br><code class="import-url">53d75d66-55c1-4768-93f5-4942956154ba</code></pre>
+                    <div class="run"></div>
+                </div>
+            </div>
+        </section>
+        <footer>
+            <div class="wrapper">
+                <p><span class="line"></span></p>
+                <p><em>Mari05liM</em> | <a href="mailto:mariodev@outlook.com.br">mariodev@outlook.com.br</a></p>
+                <p style="font-size:20px"></p>
+                <p style="margin-top: 2rem;">
+                    <button class="instatus"><span class="status-dot" style="background-color: rgb(15, 186, 85);"></span><span class="status-text">Online</span></button>
+                </p>
+            </div>
+        </footer>
+    </div>
+</body>
+
 </html>
